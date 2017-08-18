593fe96e6d52e31c1c7576237561408cbe6973ff
Patty-Chen <1093832480@qq.com>
2017-08-18 15:05:53 +0800

    init 创建三个组件 顶栏 编辑部分和预览部分

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+    <Topbar/>
+    <ResumeEditor/>
+    <ResumePreview/>
+  </div>
+</template>
+
+<script>
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        text: '你好'
+      }
+    },
+    components: {Topbar, ResumeEditor, ResumePreview}
+  }
+</script>
+
+<style>
+  p{color: red;}
+</style>
