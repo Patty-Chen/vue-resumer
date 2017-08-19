291673fc5c0f29d125f8b53d32ad36d2f496f09f
Patty-Chen <1093832480@qq.com>
2017-08-19 15:30:24 +0800

    页面基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 
 <template>
-  <div>
-    <Topbar/>
-    <ResumeEditor/>
-    <ResumePreview/>
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
@@ -11,6 +15,8 @@
   import Topbar from './components/Topbar'
   import ResumeEditor from './components/ResumeEditor'
   import ResumePreview from './components/ResumePreview'
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
   export default {
     name: 'app',
     data: function () {
@@ -21,7 +27,28 @@
     components: {Topbar, ResumeEditor, ResumePreview}
   }
 </script>
-
 <style>
-  p{color: red;}
+  .page{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+  .page>main{
+    flex-grow: 1;
+  }
+  .page>main{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin: 0;
+    display: flex;
+    justify-content: space-around;
+  }
+  #resumeEditor{
+    width: 35%;
+    background: #444;
+  }
+  #resumePreview{
+    width: 61.66667%;
+    background: #777;
+  }
 </style>
