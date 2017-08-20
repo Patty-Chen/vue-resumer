f2b30341fcbd1dcabeae6e9869de64d7d9635a5d
Patty-Chen <1093832480@qq.com>
2017-08-20 13:05:37 +0800

    在编辑部分左侧添加tab切换

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,17 +1,66 @@
 <template>
   <div id="resumeEditor">
-    I am resumeEditor
+    <nav>
+      <ol>
+        <li v-for="(item,index) in resume.displayItems"
+            :class="{active: item === selected}"
+            @click="selected = item"
+        >
+          {{index}}
+        </li>
+      </ol>
+    </nav>
+    <ol class="panels">
+      <li v-for="item in resume.displayItems" v-show="item === selected">
+        {{resume[item]}}
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'ResumeEditor'
+    name: 'ResumeEditor',
+    data () {
+      return {
+        selected: 'personal_info',
+        resume: {
+          displayItems: ['personal_info', 'work_history', 'education', 'projects', 'awards', 'contacts', 'others'],
+          personal_info: {
+            name: '',
+            city: '',
+            title: ''
+          },
+          work_history: ['test'],
+          education: [],
+          projects: [],
+          awards: [],
+          contacts: [],
+          others: []
+        }
+      }
+    }
   }
 </script>
 
-<style>
-  #resumeEditor {
-    color: red;
+<style lang="scss">
+  #resumeEditor{
+    display: flex;
+
+    > nav{
+      width: 80px;
+      > ol {
+        > li{
+          height: 48px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          &.active{
+            background: white;
+            color: black;
+          }
+        }
+      }
+    }
   }
 </style>
