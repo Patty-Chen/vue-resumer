48645955cb2a310bb6baad50d59a965137914760
Patty-Chen <1093832480@qq.com>
2017-08-20 12:28:50 +0800

    补充修改 将Topbar样式代码改为scss 添加预览和保存按钮

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,10 @@
   <div id="topbar">
     <div class="wrapper">
       <span class="logo">Resumer</span>
+      <div class="actions">
+        <button class="primary">保存</button>
+        <button>预览</button>
+      </div>
     </div>
   </div>
 </template>
@@ -16,20 +20,20 @@
   #topbar{
     background:#ffffff;
     /*box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);*/
-  }
-  #topbar>.wrapper{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin: 0 auto;
-    height:64px;
-  }
-  #topbar>.wrapper{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  #topbar .logo{
-    font-size:24px;
-    color:#000000;
+    >.wrapper{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height:64px;
+    }
+    >.wrapper{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .logo {
+      font-size: 24px;
+      color: #000000;
+    }
   }
 </style>
