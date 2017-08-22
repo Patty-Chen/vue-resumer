01d5ca3458996ef03fc5686a404fb2648e69e113
Patty-Chen <1093832480@qq.com>
2017-08-22 15:25:59 +0800

    编辑区panel完成

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -13,9 +13,13 @@
       </ol>
     </nav>
     <ol class="panels">
-      <li v-for="item in resume.displayItems" v-show="item === selected">
-        {{resume[item]}}
+      <li v-for="(item,index) in resume.displayItems" v-show="item === selected">
+        <div class="resumeField" v-for="(value,key) in resume[item]">
+          <label> {{key}} </label>
+          <input type="text" v-model="resume[item][key]">
+        </div>
       </li>
+
     </ol>
   </div>
 </template>
@@ -34,9 +38,9 @@
             city: '',
             title: ''
           },
-          work_history: ['test'],
-          education: [],
-          projects: [],
+          work_history: ['test', 'test', 'test'],
+          education: ['test', 'test', 'test'],
+          projects: ['test', 'test', 'test'],
           awards: [],
           contacts: [],
           others: []
@@ -48,16 +52,22 @@
 
 <style lang="scss">
   #resumeEditor{
+    background:#ffffff;
+    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
     display: flex;
+    flex-direction: row;
+    overflow: auto;
     > nav{
       width: 80px;
+      background: black;
       > ol {
         > li{
-
           height: 48px;
           display: flex;
           justify-content: center;
           align-items: center;
+          margin-top: 16px;
+          margin-bottom: 16px;
           .icon {
             width: 2em;
             height: 2em;
@@ -73,6 +83,33 @@
           }
         }
       }
+    }
+    > .panels{
+      flex-grow: 1;
+      > li {
+        padding: 24px;
+      }
+    }
+    ol{
+      list-style: none;
+    }
+    .resumeField{
+      > label{
+        display: block;
+      }
+      input[type=text]{
+        margin: 16px 0;
+        border: 1px solid #ddd;
+        box-shadow:inset 0 1px 3px 0 rgba(0,0,0,0.25);
+        width: 100%;
+        height: 40px;
+        padding: 0 16px;
+      }
+    }
+    hr{
+      border: none;
+      border-top: 1px solid #ddd;
+      margin: 24px 0;
     }
   }
 </style>
